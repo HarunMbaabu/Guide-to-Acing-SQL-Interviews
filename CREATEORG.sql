302d7bb1e842fdc93a82984115a9c53aa01c3163
Lux Tech Academy <51136314+LuxTechAcademy@users.noreply.github.com>
2021-02-19 10:56:11 +0300

    Update CREATEORG.sql

--- CREATEORG.sql
+++ CREATEORG.sql
@@ -13,14 +13,14 @@ CREATE TABLE Worker (
 
 INSERT INTO Worker 
 	(WORKER_ID, FIRST_NAME, LAST_NAME, SALARY, JOINING_DATE, DEPARTMENT) VALUES
-		(001, 'Monika', 'Arora', 100000, '14-02-20 09.00.00', 'HR'),
-		(002, 'Niharika', 'Verma', 80000, '14-06-11 09.00.00', 'Admin'),
-		(003, 'Vishal', 'Singhal', 300000, '14-02-20 09.00.00', 'HR'),
-		(004, 'Amitabh', 'Singh', 500000, '14-02-20 09.00.00', 'Admin'),
-		(005, 'Vivek', 'Bhati', 500000, '14-06-11 09.00.00', 'Admin'),
-		(006, 'Vipul', 'Diwan', 200000, '14-06-11 09.00.00', 'Account'),
-		(007, 'Satish', 'Kumar', 75000, '14-01-20 09.00.00', 'Account'),
-		(008, 'Geetika', 'Chauhan', 90000, '14-04-11 09.00.00', 'Admin');
+		(001, 'Harun', 'Mwenda', 100000, '14-02-20 09.00.00', 'HR'),
+		(002, 'Tim', 'Timothy', 80000, '14-06-11 09.00.00', 'Admin'),
+		(003, 'Winnie', 'Mande;a', 300000, '14-02-20 09.00.00', 'HR'),
+		(004, 'Mary', 'Wanjiku', 500000, '14-02-20 09.00.00', 'Admin'),
+		(005, 'Dennis', 'Mutwiri', 500000, '14-06-11 09.00.00', 'Admin'),
+		(006, 'jeo', 'June', 200000, '14-06-11 09.00.00', 'Account'),
+		(007, 'John', 'Doe', 75000, '14-01-20 09.00.00', 'Account'),
+		(008, 'Martin', 'Austine', 90000, '14-04-11 09.00.00', 'Admin');
 
 CREATE TABLE Bonus (
 	WORKER_REF_ID INT,
